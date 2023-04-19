3c88c24aaf7077a9f1a32bf6d5e2597778e062c7
javier.luna@wgu.edu <javier.luna@wgu.edu>
2023-04-19 16:32:24 -0600

    Initial Commit

---
+++ iosCircleWgu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosCircleWgu
+//
+//  Created by Real Jose Javier Del Mar Luna Mendoza on 19/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
