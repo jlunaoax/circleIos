78ac4eee1e04261387675f607d673ac91a734a29
javier.luna@wgu.edu <javier.luna@wgu.edu>
2023-04-27 11:58:26 -0600

    sync github with local xcode

--- iosCircleWgu/ViewController.swift
+++ iosCircleWgu/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var value = 0;
+    
+    @IBOutlet weak var btnDecrementar: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnDecrementar.isEnabled = false;
+        showMessage(value: value);
     }
 
-
+    @IBAction func btnIncrement(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        value = value + 1;
+        btnDecrementar.isEnabled = true;
+        showMessage(value: value);
+    }
+    
+    @IBAction func btnDecrement(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        if (value > 0) {
+            value = value - 1;
+        }
+        if (value == 0 ) {
+            btnDecrementar.isEnabled = false;
+        }
+        showMessage(value: value);
+    }
+    
+    func showMessage(value: Int) {
+        myLabel.text="Current value = \(value)";
+    }
 }
 
